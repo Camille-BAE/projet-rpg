9b0dd75ce2ae369381636056a7ddff668285ef7b
Camille-BAE <Camille.jbae@gmail.com>
2022-04-14 11:57:51 +0200

    add import of all the characters in game.js before launch

--- game.js
+++ game.js
@@ -1,1 +1,6 @@
-import Character from './character.js';
+import Fighter from './fighter.js';
+import Paladin from './paladin.js';
+import Monk from './monk.js';
+import Berzerker from './berzerker.js';
+import Assassin from './assassin.js';
+
